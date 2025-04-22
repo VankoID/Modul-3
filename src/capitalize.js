ea3ce7db2d185e3891277543bc38ad9225c17076
Ivan Dudarev <dudarev.id@gmail.com>
2025-04-22 15:19:40 +0400

    tests

---
+++ src/capitalize.js
@@ -0,0 +1,14 @@
+const capitalize = (text) => {
+  if (text === '') {
+    return '';
+  }
+  const firstChar = text[0].toUpperCase();
+  const restSubstring = text.slice(1);
+  return `${firstChar}${restSubstring}`;
+};
+
+console.log(capitalize('day'));
+console.log(capitalize('how are you'));
+console.log(capitalize(''));
+
+export default capitalize;
